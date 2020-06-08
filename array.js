079ca1b14d0a5826e54f17d14fcc9dd72b9239f8
M1NH42 <minhazalam365@gmail.com>
2020-06-08 15:07:58 +0530

    Manipulate array with pop() and push()

--- array.js
+++ array.js
@@ -4,4 +4,12 @@ var myList = ["Minhaz", 22, "Male", "minhaz.nith@gmail.com"];
 
 myList[3] = "minhazalam365@gmail.com"; // can be modified unlike srings
 
+// manipulate arrays with .push() and .pop()
+myList.push(["Archit", 50]);
+
+// pop() removes elemnt present at the last index
+// and saves it the variable someList
+var someList = myList.pop();
+
 console.log(myList);
+console.log(someList);
