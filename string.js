ee4ff02c1889be3f9bc117f329711c99991d0af2
M1NH42 <minhazalam365@gmail.com>
2020-06-08 14:47:11 +0530

    Find length of the string

---
+++ string.js
@@ -0,0 +1,11 @@
+var firstName = "Minhaz ";
+var lastName = "Alam";
+
+var fullName = firstName + lastName;
+
+var nameLength = fullName.length;
+
+console.log(nameLength);
+
+/* In the beow section we'll access string pos using brackets */
+console.log(lastName[3]);
