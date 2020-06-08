3328447912c14ad98613a4cc26130f51f6fdf935
M1NH42 <minhazalam365@gmail.com>
2020-06-08 14:59:44 +0530

    Add array basic fn

--- array.js
+++ array.js
@@ -1,1 +1,7 @@
 /* In this program we'll implement arrays in javscript */
+// array in js stores different elements of different data types
+var myList = ["Minhaz", 22, "Male", "minhaz.nith@gmail.com"];
+
+myList[3] = "minhazalam365@gmail.com"; // can be modified unlike srings
+
+console.log(myList);
