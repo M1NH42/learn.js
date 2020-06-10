304683ec777c52b42f13754e45cafa6414d45c56
M1NH42 <minhazalam365@gmail.com>
2020-06-10 12:43:05 +0530

    Add nested arrays

--- shopping_list.js
+++ shopping_list.js
@@ -1,1 +1,16 @@
 /* We'll be implementing the shopping list in javascript */
+// nestes arrays : array of arrays
+
+var myList = [
+  ["milk", 2],
+  ["tomatoes", 3],
+  ["onion", 1],
+  ["eggs", 12],
+  ["bread", 2],
+];
+
+// adds at the end of the list
+myList.push(["butter", 2]);
+
+// nested array
+console.log(myList[myList.length - 1]);
