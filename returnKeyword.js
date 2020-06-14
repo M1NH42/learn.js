e31038c955ddcd3ad3fea13bc811ff43fe8ffc8c
M1NH42 <minhazalam365@gmail.com>
2020-06-14 10:18:49 +0530

    Add return keyword

--- returnKeyword.js
+++ returnKeyword.js
@@ -1,1 +1,13 @@
 /* In this program we'll be using return keyword to return a value from a function */
+
+// use of return function to return the operation
+function useReturnKeyword(a, b) {
+  // returns sum of the two numbers
+  return a + b;
+}
+
+// call the above function and store the return value in the variable returnValue
+var returnValue = useReturnKeyword(1234, 1234);
+
+// console.log(useReturnKeyword(123, 123));
+console.log(returnValue);
