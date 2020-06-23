1b9a27dd8a1df0f1f598c8e8878dcd8254270446
M1NH42 <minhazalam365@gmail.com>
2020-06-23 12:23:00 +0530

    Add template literals

--- string_interpolation.js
+++ string_interpolation.js
@@ -1,2 +1,12 @@
 // In this module we'll be implementing string and interpolation included
 // in ES 6
+const language = "English";
+
+console.log(language.includes("sh")); // returns true
+
+console.log(language.startsWith("E"));
+console.log(language.endsWith("h"));
+console.log(language.repeat(3));
+
+// we willbe template literals in ES6
+console.log(`I speak ${language === "English" ? "British English" : "None"}`);
