521e62895ee15ffda566dae3f3cd2409b26f654a
M1NH42 <minhazalam365@gmail.com>
2020-06-23 11:40:20 +0530

    Add let and const keywords

---
+++ const_imm.js
@@ -0,0 +1,16 @@
+// Task 3: Variable const and immutability
+
+// let keyword will not let us redeclare the variable again
+// that is not the case with the var keyword
+
+// let paintingName = "The Starry Nights";
+// console.log(paintingName);
+
+// let paintingName = "The Night Cafe";
+// console.log(paintingName);
+
+const paintingName = "the starry nights";
+console.log(paintingName);
+
+paintingName = "the night cafe";
+console.log(paintingName);
