d5ef219b4992bdf89fa2f1fd2e41757f289d52ba
M1NH42 <minhazalam365@gmail.com>
2020-06-23 13:12:47 +0530

    Add some extra codes for practice

--- arrow_this.js
+++ arrow_this.js
@@ -1,1 +1,12 @@
 // We will be implementing arrow functions and this functions
+// console.log(breed);
+// const breed = "Cocker Spaniel";
+
+const p = {
+  name: "d",
+  age: 23,
+};
+
+Object.freeze(p);
+p.age = 24;
+console.log(p);
---
+++ q.js
@@ -0,0 +1,4 @@
+const sp = ["a", "b", "c", "d"];
+const [first, second, ...other] = sp;
+
+console.log(first, second, other);
